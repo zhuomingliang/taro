b24ea6e5616bcd00506849dd79283cad6c4dd170
yuche <i@yuche.me>
2018-12-26 11:38:06 +0800

    refactor(transformer): 自定义 render 函数与导出类静态属性的联系

--- packages/taro-transformer-wx/src/render.ts
+++ packages/taro-transformer-wx/src/render.ts
@@ -23,7 +23,7 @@ import {
   getSlotName,
   replaceJSXTextWithTextComponent
 } from './utils'
-import { difference, get as safeGet, cloneDeep } from 'lodash'
+import { difference, get as safeGet, cloneDeep, uniq } from 'lodash'
 import {
   setJSXAttr,
   buildBlockElement,
@@ -117,6 +117,7 @@ export class RenderParser {
   private usedThisProperties = new Set<string>()
   private incrementCalleeId = incrementId()
   private classComputedState = new Set<string>()
+  private isDefaultRender: boolean = false
 
   private renderPath: NodePath<t.ClassMethod>
   private methods: ClassMethodsMap
@@ -284,6 +285,9 @@ export class RenderParser {
   }
 
   setProperies () {
+    if (!this.isDefaultRender) {
+      return
+    }
     const properties: t.ObjectProperty[] = []
     this.componentProperies.forEach((propName) => {
       properties.push(
@@ -1019,7 +1023,8 @@ export class RenderParser {
     usedState: Set<string>,
     customComponentNames: Set<string>,
     componentProperies: Set<string>,
-    loopRefs: Map<t.JSXElement, LoopRef>
+    loopRefs: Map<t.JSXElement, LoopRef>,
+    methodName: string
   ) {
     this.renderPath = renderPath
     this.methods = methods
@@ -1031,6 +1036,7 @@ export class RenderParser {
     this.loopRefs = loopRefs
     const renderBody = renderPath.get('body')
     this.renderScope = renderBody.scope
+    this.isDefaultRender = methodName === 'render'
 
     const [, error] = renderPath.node.body.body.filter(s => t.isReturnStatement(s))
     if (error) {
@@ -1418,12 +1424,22 @@ export class RenderParser {
 
   setCustomEvent () {
     const classPath = this.renderPath.findParent(isClassDcl) as NodePath<t.ClassDeclaration>
-    let classProp = t.classProperty(t.identifier('$$events'), t.arrayExpression(Array.from(this.usedEvents).map(s => t.stringLiteral(s)))) as any // babel 6 typing 没有 static
-    classProp.static = true
-    classPath.node.body.body.unshift(classProp)
+    const eventPropName = '$$events'
+    const body = classPath.node.body.body.find(b => t.isClassProperty(b) && b.key.name === eventPropName) as t.ClassProperty
+    const usedEvents = Array.from(this.usedEvents).map(s => t.stringLiteral(s))
+    if (body && t.isArrayExpression(body.value)) {
+      body.value = t.arrayExpression(uniq(body.value.elements.concat(usedEvents)))
+    } else {
+      let classProp = t.classProperty(t.identifier('$$events'), t.arrayExpression(usedEvents)) as any // babel 6 typing 没有 static
+      classProp.static = true
+      classPath.node.body.body.unshift(classProp)
+    }
   }
 
   setUsedState () {
+    if (!this.isDefaultRender) {
+      return
+    }
     for (const [ key, method ] of this.methods) {
       if (method) {
         if (method.isClassMethod()) {
